f87e712c402cceed52fd52236eb0b50df44b1033
pkmaneri <praveen.kumar.maneri@gmail.com>
2021-07-16 17:19:40 +0530

    fc

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import MyProduct from "./components/Product";
 import Employee from "./components/Employee";
 import Billing from "./components/Billing";
 import StateOne from "./components/State1";
+import stateTwo from "./components/State2";
 function App() {
   return (
     <HashRouter>
@@ -19,6 +20,8 @@ function App() {
         <li ><Link to="/myemp">Employee</Link> </li>
         <li ><Link to="/mybill">Billing</Link> </li>
         <li ><Link to="/mystate">State</Link> </li>
+        <li ><Link to="/mystatetwo">StateTwo</Link> </li>
+
 
       </ul>
       <Route exact path="/" component={Dashboard}/>
@@ -28,6 +31,8 @@ function App() {
       <Route exact path="/myemp" component={Employee}/>
       <Route exact path="/mybill" component={Billing}/>
       <Route exact path="/mystate" component={StateOne}/>
+      <Route exact path="/mystatetwo" component={stateTwo}/>
+
 
     </HashRouter>
   );
---
+++ src/components/State2.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react"
+
+export default class stateTwo extends Component {
+    constructor() {
+        super();
+        this.state = {
+            bookList: ["Java", "JavaScript", "React", "Html"],
+            newBook:"",
+            message:""
+        }
+    }
+    pickValue=(obj)=>{
+        this.setState({
+            newBook:obj.target.value
+        })
+
+    }
+   
+    save=()=>{
+    //    alert(this.state.newBook)
+    this.setState({
+        bookList:this.state.bookList.concat(this.state.newBook),
+        message: this.state.newBook+"Book Added Successfully",
+        newBook:""
+    })
+    }
+    handleDeleteBook=(id)=>{
+        // alert(id)
+        // var bookName=this.state.bookList
+        this.state.bookList.splice(id,1);//a-b
+        this.setState({
+            bookList:this.state.bookList,//a=a
+            message:"Book Delete Successfully",
+
+        })
+
+    }
+    render() {
+        return (
+            <div className="container">
+                <h1 align="center"> React  Class Component Add,List ,Delete Example</h1>
+                <h1 align="center">Total Book:{this.state.bookList.length}</h1>
+                <p align="center">
+                    Enter Book Name:<input type="text"
+                     onChange={this.pickValue} value={this.state.newBook}/>
+                    <button onClick={this.save}>Save Book</button>
+                </p>
+                <p align="center">{this.state.message}</p>
+                <table align="center" width="60%" cellPadding="10">
+                    <thead>
+                        <tr bgcolor="orange">
+                            <th>SI No</th>
+                            <th>Book Name</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody align="center">
+                        {this.state.bookList.map((bookName, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{index} </td>
+                                    <td>{bookName} </td>
+                                    <td>
+                                        <button onClick={this.handleDeleteBook.bind(this,index)}>Delete</button>
+                                         </td>
+                                </tr>
+                            )
+                        })}
+
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
